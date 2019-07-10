1b1c264cfd0547f8cfda7c5553d6d7ff44486ae6
gmidorii <stitow14@gmail.com>
2019-07-10 23:58:00 +0900

    Add hello path.

--- echo/main.go
+++ echo/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/labstack/echo/v4/middleware"
 
 	_ "github.com/gmidorii/go-sandbox/echo/docs"
 	"github.com/labstack/echo/v4"
@@ -17,12 +21,40 @@ func pingHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "OK")
 }
 
+type HelloForm struct {
+	Name string `json:"name" validate:"required"`
+}
+
+type HelloRecord struct {
+	Message string `json:"message"`
+}
+
+// helloHandler is hello path.
+// @Success 200 {string}
+func helloHandler(c echo.Context) error {
+	form := HelloForm{}
+	if err := c.Bind(&form); err != nil {
+		log.Println(err)
+		return c.String(http.StatusBadRequest, "invalid")
+	}
+	if err := c.Validate(&form); err != nil {
+		log.Println(err)
+		return c.String(http.StatusBadRequest, "invalid")
+	}
+	rec := HelloRecord{
+		Message: fmt.Sprintf("Hello %v !!", form.Name),
+	}
+	return c.JSON(http.StatusOK, &rec)
+}
+
 // @title Swagger test api
 // @version 1.0
 // @description xxxx.
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 	e.GET("/ping", pingHandler)
+	e.GET("/hello", helloHandler)
 	e.GET("/doc/*", echoSwagger.WrapHandler)
 	e.Logger.Fatal(e.Start(":9999"))
 }
